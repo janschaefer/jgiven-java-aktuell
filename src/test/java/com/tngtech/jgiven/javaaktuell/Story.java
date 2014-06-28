1c1c99acdd06b3368c7d00f57d181ee2039309f1
Jan Schäfer <jan.schaefer@tngtech.com>
2014-06-28 23:27:36 +0200

    change story tag

--- src/test/java/com/tngtech/jgiven/javaaktuell/Story.java
+++ src/test/java/com/tngtech/jgiven/javaaktuell/Story.java
@@ -1,8 +1,12 @@
 package com.tngtech.jgiven.javaaktuell;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import com.tngtech.jgiven.annotation.IsTag;
 
-@IsTag
+@IsTag(prependType=true)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Story {
     String value();
 }
